2472c2d82c9094a59c1001f3aa5ccab5fc002446
Diego Rueda <diego.rueda2@gmail.com>
2018-03-29 23:33:11 -0400

    Estructura de Panel administrativo

---
+++ resources/views/admin/tag/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DIego
+ * Date: 3/29/2018
+ * Time: 11:26 PM
+ */
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a class="nav-link" href="{{ route('blog') }}">Blog</a> </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -45,6 +45,10 @@
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                             <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                         @else
+                            <li><a class="nav-link" href="{{ route('tags.index') }}">Etiquetas</a> </li>
+                            <li><a class="nav-link" href="{{ route('categories.index') }}">Categorias</a> </li>
+                            <li><a class="nav-link" href="{{ route('posts.index') }}">Entradas</a> </li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
