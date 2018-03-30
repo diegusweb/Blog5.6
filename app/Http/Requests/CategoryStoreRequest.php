a27d9b90881df9b126aa2874f98362ed5667b460
Diego Rueda <diego.rueda2@gmail.com>
2018-03-30 17:18:25 -0400

    fix error

--- app/Http/Requests/CategoryStoreRequest.php
+++ app/Http/Requests/CategoryStoreRequest.php
@@ -26,6 +26,7 @@ class CategoryStoreRequest extends FormRequest
         return [
             'name' => 'required',
             'slug' => 'required|unique:categories,slug',
+            'body' => 'required',
         ];
     }
 }
